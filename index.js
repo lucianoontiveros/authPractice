c2c1abe8a8b50007ee68ed66b372066b73730859
Luciano Ontiveros <luciano.a.ontiveros@gmail.com>
2022-07-30 16:03:17 -0300

    practica para autentificar usuarios terminada con correciones por actualización de versión jwtwebToken

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const expressJwt = require('express-jwt')
+const jsonwebtoken = require('jsonwebtoken')
+const { expressjwt: jwt } = require("express-jwt");
 const User = require('./user')
 const app = express()
 const port = 4000
@@ -11,12 +11,13 @@ require('dotenv').config()
 mongoose.connect(process.env.BASEDEDATOS)
 app.use(express.json())
 
-const singToken = _id => jwt.sign({ _id }, process.env.STRINGSECRET)
+const validateJwt = jwt({ secret: process.env.STRINGSECRET, algorithms: ["HS256"] })
+const signToken = _id => jsonwebtoken.sign({ _id }, process.env.STRINGSECRET)
 
 
-app.post('/register', async (req, res) => {
+
+app.post('/register', async (req , res) => {
     const { body }  = req
-    console.log({ body })
     try {
         const isUser = await User.findOne( {email: body.email})
         if(isUser){
@@ -25,12 +26,49 @@ app.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt()
         const hashed = await bcrypt.hash(body.password, salt);
         const user = await User.create( { email: body.email, password: hashed, salt})
-        const signed = singToken(user._id)
+        const signed = signToken(user._id)
         res.status(201).send(signed)
     } catch (err) {
         console.log(err)
         res.status(500).send(err.message)
     }
+})
+
+app.post('/login', async (req , res) => {
+    const { body } = req
+    try { 
+        const user = await User.findOne( { email: body.email})
+        if(!user){
+            res.status(403).send('usario y/o contraseña inválida')
+        } else {
+            const isMatch = await bcrypt.compare(body.password, user.password)
+            if (isMatch){
+                const signed = signToken(user._id) 
+                res.status(200).send(signed)
+            } else {
+                res.status(403).send('usuario y/o contraseña inválida')
+            }
+        }
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+})
+
+const findAndAssignUser = async (req, res, next) => {
+    try {
+        const user = await User.findOne(req.user_id)
+        if (!user){
+            return res.status(401).end()
+        }
+        req.user = user
+        next()
+    } catch (e){
+        next(e)
+    }
+}
+const autentificacion = express.Router().use(validateJwt, findAndAssignUser)
+app.get('/lele', autentificacion, async (req, res) => {
+    res.send(req.user)
 })
 
 app.listen(port, () => {
